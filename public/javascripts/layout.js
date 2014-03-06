3b4e74055c039aebe434d3772f5908d0706a1904
Daniel Schwarz <dan-schwarz@hotmail.com>
2014-03-06 00:41:42 -0500

    Completed physics sim page

--- public/javascripts/layout.js
+++ public/javascripts/layout.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
 	$("a[href='"+window.location.pathname+"']").addClass("active");
-})
+
+	$('.noScroll').hover(function (){
+	    $('body').css('overflow','hidden');
+	}, function (){
+	    $('body').css('overflow','auto');
+	})
+});
